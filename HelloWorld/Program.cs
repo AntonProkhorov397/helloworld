17fd8bf24ca99c03d2dd51dd5f2c37fb011c2ede
Opilane TTHK <opilane@tthk.ee>
2021-11-09 14:05:52 +0200

    add string interpolation

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm ask the user to enter their first name
+            //programm greets the user using their first name
+            Console.WriteLine("enter yourfirst name:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Hello" + firstName + "!" );
+
+
+            Console.Read();
+        }
+    }
+}
